427042558507b1a119bfbbc002954ebb05fc45d1
DidiNox <akuwudikedivinegift@gmail.com>
2023-09-08 03:59:50 +0100

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - main function
- *
  * Return: the value 1.
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" 
+	write(2, "and that piece of art is useful\" 
 			- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
