b9c25fc1504ac2b4c3c3bab91d83287aab31e2de
DidiNox <akuwudikedivinegift@gmail.com>
2023-09-08 03:42:49 +0100

    Corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main function
- * 
+ *
  * Return: the value 0
  */
 int main(void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,16 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	int d;
+	char c;
+	long int a;
+	long long int b;
+	float f;
+
+	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(d));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(unsigned a));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(unsigned b));
+	printf("Size of a float: %f byte(s)\n", sizeof(f));
+	return (0);
 }
